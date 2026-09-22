d49cf850ddb9a1d7a03275ef19e0351b1b60c0ab
jacobfriisstrand <jacob@friis.com>
2026-09-22 11:19:10 +0300

    fix(sidebar): remove unnecessary padding from SidebarHeader component

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -345,7 +345,7 @@ function SidebarInput({
 function SidebarHeader({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
-      className={cn("flex flex-col gap-2 p-2 pt-7", className)}
+      className={cn("flex flex-col gap-2 p-2", className)}
       data-sidebar="header"
       data-slot="sidebar-header"
       {...props}
